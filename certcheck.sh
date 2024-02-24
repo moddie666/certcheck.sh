fb9576c434ccd9ea599edaea4269d9b78ff94532
moddie666 <51479677+moddie666@users.noreply.github.com>
2024-02-24 16:50:58 +0100

    change some things

--- certcheck.sh
+++ certcheck.sh
@@ -23,10 +23,15 @@ as the first argument.
 NOTE: Only files with the following extensions are checked
       in 'nothing' or 'directory' modes:
 '*.crt' '*.pem' '*.ca' '*.intermediate' '*.key'
-All in one cerificate+key combo-files should also work."
+All in one cerificate+key combo-files should also work.
+
+POTENTIAL BUG:
+Intemediate certs, which are also checked for, are assumed to be located
+in '/sslstore/intermediate' in file mode or <dir>/intermediate in directory
+mode. If these do not exist the dir containing the cert file is checked."
 
 read_multi(){
-		mcount="$(grep -c "BEGIN CERTIFICATE--" $1)"
+    mcount="$(grep -c "BEGIN CERTIFICATE--" $1)"
     echo -e "\e[${2}m#--- $mcount Certs in $1 ---#\e[0m"
     awk 'split_after == 1 {n++;split_after=0}; /-----END CERTIFICATE-----/{split_after=1}; {print > "/tmp/cert" n ".tmp"}' < $1
     for tempf in $(ls -A /tmp/cert*.tmp | sort -V)
@@ -35,7 +40,7 @@ read_multi(){
        echo -e "$CI1"
        rm $tempf
     done
-		veri=$(openssl verify -CAfile $1 $1 2>/dev/null || echo 'FAILED')
+    veri=$(openssl verify -CAfile $1 $1 2>/dev/null || echo 'FAILED')
     echo -e "\e[${2}m---\nVerification:\e[0m $veri"
 }
 read_single(){
@@ -45,36 +50,36 @@ read_single(){
 }
 
 read_cert(){
-	  keysearch=0
-	  ccount="$(grep -c "BEGIN CERTIFICATE--" $cfile)"
+    keysearch=0
+    ccount="$(grep -c "BEGIN CERTIFICATE--" $cfile)"
     if [ "$ccount" -gt "1" ]
     then read_multi $cfile '33'
-		     find_key
+         find_key
     elif [ "$ccount" -eq "1" ]
     then read_single $cfile '32'
          find_chain
          find_key
-	  else echo "$cfile does not look like a cert !"
-	       return 1
+    else echo "$cfile does not look like a cert !"
+         return 1
     fi
     #read -p 'next'
     echo
 }
 
 find_chain(){
-   	if [ -d /srv/SSLstore/intermediate ]
-    then sslstore="/srv/SSLstore/intermediate"
+    if [ -d "$sslstore" ]
+    then sslstore="/sslstore/intermediate"
     else sslstore=""
     fi
     CAFILES=$(find ${sslstore} ${wdir})
     echo "Looking for matching intermediates: ${sslstore} ${wdir} "
-	  for ca in $CAFILES
+    for ca in $CAFILES
     do if [ "$ca" != "$cfile" ]
-		   then openssl verify -CAfile $ca $cfile &>/dev/null
+       then openssl verify -CAfile $ca $cfile &>/dev/null
             if [ $? -eq 0 ]
             then if [ "$(grep -c BEGIN $ca)" -gt "1" ]
                  then read_multi $ca '94'
-                 else	read_single $ca '96'
+                 else read_single $ca '96'
                  fi
             fi
        fi
@@ -103,11 +108,12 @@ case $1 in
      -h|--help) echo "$USAGE"
                 exit 0;;
 esac
-
+sslstore="/sslstore/intermediate"
 if [ -f "$1" ]
 then FILES="$@"
 elif [ -d "$1" ]
 then FILES="$(find $@ -maxdepth 1 -iname '*.crt' -o -iname '*.pem' -o -iname '*.ca' -o -iname '*.intermediate')"
+     sslstore="$1/intermediate"
 else FILES="$(find . -maxdepth 1 -iname '*.crt' -o -iname '*.pem' -o -iname '*.ca' -o -iname '*.intermediate')"
 fi
 
